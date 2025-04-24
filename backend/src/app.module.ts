ace7d0403411169e433e130fc2b44d4578ba09fd
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-24 14:25:10 -0300

    feat: products resource archives transferred to dir resources

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductsModule } from './products/products.module';
+import { ProductsModule } from './resources/products/products.module';
 
 @Module({
     imports: [ProductsModule],
