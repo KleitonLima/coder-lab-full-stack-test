36b6963fe6255fe3e605d5c8adad1eb7a97cbcfe
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-26 00:15:41 -0300

    feat(main): enhance bootstrap function with CORS support and global validation pipes

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { setupDbConnection } from './config/connection.db';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
+import { swaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+        cors: true,
+    });
+    app.useGlobalPipes(new ValidationPipe());
     setupDbConnection();
+    swaggerConfig(app);
+    await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
