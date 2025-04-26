e03ac06a7d1adeb99e2f090bcf0538614650c5a5
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-26 13:55:22 -0300

    feat(product): implement Product entity with validation and relationships

--- backend/src/resources/products/entities/product.entity.ts
+++ backend/src/resources/products/entities/product.entity.ts
@@ -1,1 +1,32 @@
-export class Product {}
+import { IsInt, IsNumber, IsString } from 'class-validator';
+import { Category } from 'src/resources/category/entities/category.entity';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('products')
+export class Product {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @IsString({ each: true })
+    @Column({ type: 'simple-array' })
+    categories_id: string[];
+
+    @OneToMany(() => Category, (category) => category)
+    categories: Category[];
+
+    @IsString()
+    @Column({ type: 'varchar', length: 255 })
+    name: string;
+
+    @IsInt()
+    @Column({ type: 'int' })
+    qty: number;
+
+    @IsNumber()
+    @Column({ type: 'decimal', precision: 10, scale: 2 })
+    price: number;
+
+    @IsString()
+    @Column({ type: 'varchar', length: 2000 })
+    photo: string;
+}
