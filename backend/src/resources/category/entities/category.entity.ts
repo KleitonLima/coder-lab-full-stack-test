33f19ed746beceacbfd8ff62dfc0194c2cb98224
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-26 10:46:32 -0300

    feat(category): define Category entity with properties and relationships

--- backend/src/resources/category/entities/category.entity.ts
+++ backend/src/resources/category/entities/category.entity.ts
@@ -1,1 +1,17 @@
-export class Category {}
+import { IsString } from 'class-validator';
+import { Column, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+export class Category {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @ManyToOne(() => Category || null, {
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+    })
+    parent: Category | null;
+
+    @IsString()
+    @Column({ type: 'varchar', length: 255 })
+    name: string;
+}
