7ceae3bfe4598239d602dfc5ed8d9829f14cff20
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-26 09:11:13 -0300

    feat(app): update getHello method to getAppStatus and modify response message

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { ENVCONFIG } from './config/env.config';
 
 @Injectable()
 export class AppService {
-    getHello(): string {
-        return 'Hello World!';
+    getAppStatus(): string {
+        return `Server is running! 🚀\n Please check <a href="http://localhost:${ENVCONFIG.PORT}/docs">http://localhost:${ENVCONFIG.PORT}/docs</a> for Swagger docs...`;
     }
 }
