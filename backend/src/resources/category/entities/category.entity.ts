676d084d65ead8c445afe9f95d0daa1466ffa012
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-26 10:49:08 -0300

    feat(category): add Entity decorator to Category class

--- backend/src/resources/category/entities/category.entity.ts
+++ backend/src/resources/category/entities/category.entity.ts
@@ -1,6 +1,7 @@
 import { IsString } from 'class-validator';
-import { Column, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity('categories')
 export class Category {
     @PrimaryGeneratedColumn('uuid')
     id: string;
