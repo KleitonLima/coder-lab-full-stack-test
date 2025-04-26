ef28f0a711e9deb4a89454125fa0046a43e87e84
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-25 23:34:36 -0300

    feat(main): call setupDbConnection in bootstrap function for database initialization

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { setupDbConnection } from './config/connection.db';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     await app.listen(process.env.PORT ?? 3000);
+    setupDbConnection();
 }
 bootstrap();
