87e64f23471d57206c09b95b65b0fcc3eb956039
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-27 14:00:03 -0300

    refactor(product): remove class-validator decorators from Product entity

--- backend/src/resources/products/entities/product.entity.ts
+++ backend/src/resources/products/entities/product.entity.ts
@@ -1,25 +1,20 @@
-import { IsInt, IsNumber, IsString } from 'class-validator';
+import { Column, Entity, PrimaryGeneratedColumn, ManyToMany } from 'typeorm';
 import { Category } from '../../category/entities/category.entity';
-import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('products')
 export class Product {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
-    @IsString()
     @Column({ type: 'varchar', length: 255 })
     name: string;
 
-    @IsInt()
     @Column({ type: 'int' })
     qty: number;
 
-    @IsNumber()
     @Column({ type: 'decimal', precision: 10, scale: 2 })
     price: number;
 
-    @IsString()
     @Column({ type: 'varchar', length: 2000 })
     photo: string;
 
