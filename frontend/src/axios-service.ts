f18b1e90b90544cc7c3b0c31599a021254c6fff3
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-28 17:48:32 -0300

    feat(axios): create axiosService for API requests configuration

---
+++ frontend/src/axios-service.ts
@@ -0,0 +1,9 @@
+import axios from "axios";
+import { ENVCONFIG } from "./config/env.config";
+
+export const axiosService = axios.create({
+    baseURL: ENVCONFIG.API_URL,
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
