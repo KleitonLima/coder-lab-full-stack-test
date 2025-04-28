8b299fbb4d9d85aff91b58c901eb8f444f925a8d
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-28 19:47:53 -0300

    feat(home): add search functionality to filter products in the listing

--- frontend/src/ui/pages/Home/Home.tsx
+++ frontend/src/ui/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import { DiamondPlus } from "lucide-react";
+import { DiamondPlus, Search } from "lucide-react";
 import { Button } from "../../components/button";
 import {
     Card,
@@ -11,14 +11,16 @@ import { useEffect, useState } from "react";
 import { homeController } from "./home.controller";
 import { IAddProduct } from "./home.interfaces";
 import clsx from "clsx";
+import { Input } from "../../components/input";
 
 export const Home = () => {
     const navigate = useNavigate();
     const [products, setProducts] = useState<IAddProduct[]>([]);
+    const [search, setSearch] = useState("");
 
     const handleAllProducts = async () => {
         try {
-            const response = await homeController.getAllProducts();
+            const response = await homeController.getAllProducts(search);
 
             if (response.status === 200) {
                 setProducts(response.data as IAddProduct[]);
@@ -31,15 +33,33 @@ export const Home = () => {
     useEffect(() => {
         handleAllProducts();
     }, []);
+
+    const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { value } = event.target;
+        setSearch(value);
+    };
+
     return (
         <main>
-            <header>
+            <header className="flex justify-between p-4">
                 <Button
-                    className="bg-blue-700"
+                    className="w-50 bg-blue-700"
                     onClick={() => navigate("/add-product")}
                 >
                     <DiamondPlus /> Adicionar produto
                 </Button>
+                <div className="flex items-center">
+                    <button onClick={handleAllProducts}>
+                        <Search className="w-9 h-9 p-1 border-b-2 border-t-2 border-l-2 border-sky-700 rounded-l-md" />
+                    </button>
+                    <Input
+                        type="text"
+                        placeholder="Digite o nome do produto"
+                        className="w-48 border-b-2 border-t-2 border-r-2 border-l-0 rounded-l-none border-sky-700"
+                        value={search}
+                        onChange={handleSearch}
+                    />
+                </div>
             </header>
             <div className="grid grid-cols-1 gap-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 py-6 place-items-center">
                 {products.map((product) => (
@@ -59,7 +79,8 @@ export const Home = () => {
                         </CardHeader>
                         <CardContent>
                             <p>
-                                R${product.price.toString().replace(".", ",")}
+                                R$
+                                {product.price.toString().replace(".", ",")}
                             </p>
                             {product.qty <= 0 && <p>Produto esgotado</p>}
                         </CardContent>
