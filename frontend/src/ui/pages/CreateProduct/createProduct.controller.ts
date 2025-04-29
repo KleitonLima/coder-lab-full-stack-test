93527a6e10823a28d7f4aa537c84e049861ca86d
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-28 22:25:01 -0300

    fix(controller): rename createProduct to createProductController for consistency

--- frontend/src/ui/pages/CreateProduct/createProduct.controller.ts
+++ frontend/src/ui/pages/CreateProduct/createProduct.controller.ts
@@ -1,7 +1,7 @@
 import { api } from "../../../services/axios.service";
 import { IProduct } from "../../interfaces";
 
-export const createProduct = {
+export const createProductController = {
     addProduct(data: IProduct) {
         return api.post("/products", data);
     },
