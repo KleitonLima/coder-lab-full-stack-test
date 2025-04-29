ff310d70be6b331b16a9cfd212f72ca52ca0bf0a
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-28 22:50:03 -0300

    feat(page): implement CreateProduct page with form and category selection

---
+++ frontend/src/ui/pages/CreateProduct/CreateProduct.tsx
@@ -0,0 +1,167 @@
+import { ChevronLeft } from "lucide-react";
+import { Button } from "../../components/button";
+import { useNavigate } from "react-router-dom";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "../../components/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { formSchema } from "./createProductForm.schema";
+import { z } from "zod";
+import { Input } from "../../components/input";
+import { useEffect, useState } from "react";
+import { ICategory } from "../../interfaces";
+import { Checkbox } from "../../components/checkbox";
+import { createProductController } from "./createProduct.controller";
+
+export const CreateProduct = () => {
+    const navigate = useNavigate();
+    const [categories, setCategories] = useState<ICategory[]>([]);
+
+    const handleGetCategories = async () => {
+        try {
+            const response = await createProductController.getCategories();
+            if (response.status === 200) {
+                setCategories(response.data as ICategory[]);
+            }
+        } catch (error) {
+            console.error("Error fetching categories:", error);
+        }
+    };
+
+    useEffect(() => {
+        handleGetCategories();
+    }, []);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: { categories: [] },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const response = await createProductController.addProduct(values);
+
+            if (response.status === 201) {
+                navigate("/");
+            }
+        } catch (error) {
+            console.error("Error creating product:", error);
+        }
+    };
+
+    return (
+        <main>
+            <header>
+                <Button className="bg-blue-700" onClick={() => navigate("/")}>
+                    <ChevronLeft />
+                </Button>
+            </header>
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(onSubmit)}
+                    className="space-y-8"
+                >
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Nome do produto</FormLabel>
+                                <FormControl>
+                                    <Input {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="qty"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Quantidade</FormLabel>
+                                <FormControl>
+                                    <Input type="number" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="price"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Preço</FormLabel>
+                                <FormControl>
+                                    <Input type="number" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="photo"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Link da foto</FormLabel>
+                                <FormControl>
+                                    <Input type="url" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    {categories.map((category) => (
+                        <FormField
+                            key={category.id}
+                            control={form.control}
+                            name="categories"
+                            render={({ field }) => {
+                                return (
+                                    <FormItem
+                                        key={category.id}
+                                        className="flex flex-row items-start space-x-3 space-y-0"
+                                    >
+                                        <FormControl>
+                                            <Checkbox
+                                                checked={field.value?.includes(
+                                                    category.id
+                                                )}
+                                                onCheckedChange={(checked) => {
+                                                    return checked
+                                                        ? field.onChange([
+                                                              ...field.value,
+                                                              category.id,
+                                                          ])
+                                                        : field.onChange(
+                                                              field.value?.filter(
+                                                                  (value) =>
+                                                                      value !==
+                                                                      category.id
+                                                              )
+                                                          );
+                                                }}
+                                            />
+                                        </FormControl>
+                                        <FormLabel className="text-sm font-normal">
+                                            {category.name}
+                                        </FormLabel>
+                                    </FormItem>
+                                );
+                            }}
+                        />
+                    ))}
+                    <Button type="submit">Submit</Button>
+                </form>
+            </Form>
+        </main>
+    );
+};
