412904a530c158f6fa37d8addbf26a7919a3eabb
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-28 21:31:02 -0300

    refactor(home.controller): remove addProduct method from homeController

--- frontend/src/ui/pages/Home/home.controller.ts
+++ frontend/src/ui/pages/Home/home.controller.ts
@@ -1,11 +1,6 @@
 import { api } from "../../../services/axios.service";
-import { IAddProduct } from "./home.interfaces";
 
 export const homeController = {
-    addProduct(data: IAddProduct) {
-        return api.post("/products", data);
-    },
-
     getAllProducts(search?: string) {
         const query = search ? `?search=${search}` : "";
         return api.get(`/products${query}`);
