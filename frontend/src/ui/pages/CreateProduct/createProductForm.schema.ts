b58acfbdf2d2ba7e04805bbf896d4f6b7e00dda7
Kleiton Lima <kleiton.mini@gmail.com>
2025-04-28 22:44:27 -0300

    fix(schema): coerce qty and price to numbers in form schema

--- frontend/src/ui/pages/CreateProduct/createProductForm.schema.ts
+++ frontend/src/ui/pages/CreateProduct/createProductForm.schema.ts
@@ -2,8 +2,8 @@ import { z } from "zod";
 
 export const formSchema = z.object({
     name: z.string().min(3).max(50),
-    qty: z.number().min(1).positive(),
-    price: z.number().positive(),
+    qty: z.coerce.number().min(1).positive(),
+    price: z.coerce.number().positive(),
     photo: z.string().url(),
     categories: z
         .array(z.string())
